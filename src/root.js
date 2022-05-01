57b530e03f3ce3a692d7dcc53ac89cac387a33cf
Don Romaniello <don.romaniello@gmail.com>
2022-05-01 10:35:48 -0400

    Buggy but possible mouse.

--- src/root.js
+++ src/root.js
@@ -4,17 +4,16 @@ import useMouse from '@react-hook/mouse-position'
 export default function Root() {
   const target = React.useRef(null);
   const mouse = useMouse(target, {
-    fps: 30,
+    fps: 15,
     enterDelay: 0,
-    leaveDelay: 0
+    leaveDelay: 100
   });
 
   return (
-    <div ref={target}>
-      <div class="demo" style={{top:mouse.y, left:mouse.x}}>
+    <div ref={target} style={{width: "100%", height: "100%"}}>
+      <div class="demo" style={{top:mouse.y - 100, left:mouse.x - 100}}>
         <div>Demo</div>
       </div>
-      <br />
     </div>
   );
 }
