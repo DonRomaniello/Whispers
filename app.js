39123796dc5df37c5152a24858a1bf7ce857d93a
Don Romaniello <don.romaniello@gmail.com>
2022-05-01 17:48:33 -0400

    Decrufting.

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const app = express()
 const port = 3000
 const path = require('path');
 require('dotenv').config()
-// const { db, Cat, Weight, Food } = require('../client/database')
+const { db, User } = require('./client/database')
 
 app.use(express.static('public'));
+
+app.get('/api/users', async(req, res, next) => {
+  try {
+    const users = await User.findAll()
+    console.log(users)
+    res.send(users)
+  } catch(err){
+    next(err)
+  }
+})
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
