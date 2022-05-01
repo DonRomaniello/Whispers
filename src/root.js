f07fbb8d52a2131ddcf18a317f339faf90e3cc27
Don Romaniello <don.romaniello@gmail.com>
2022-05-01 15:34:49 -0400

    No use for mouse redux.

--- src/root.js
+++ src/root.js
@@ -2,14 +2,20 @@ import * as React from 'react'
 import useMouse from '@react-hook/mouse-position'
 
 export default function Root() {
+
+
   const target = React.useRef(null);
+
   const mouse = useMouse(target, {
     fps: 15,
     enterDelay: 0,
     leaveDelay: 100
   });
 
+  // const mousePositon =
+
   return (
+    console.log("here")
     <div ref={target} style={{width: "100%", height: "100%"}}>
       <div class="demo" style={{top:mouse.y - 100, left:mouse.x - 100}}>
         <div>Demo</div>
@@ -17,5 +23,3 @@ export default function Root() {
     </div>
   );
 }
-
-
